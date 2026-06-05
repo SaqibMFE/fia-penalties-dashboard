66857726fc6397f9670fb4058b58a07304f60cd3
SaqibMFE <115488241+SaqibMFE@users.noreply.github.com>
2026-06-05 18:02:25 +0100

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,12 +2,13 @@
 import streamlit as st
 import pandas as pd
 import os
-import subprocess
 
-# Run parser if data does not exist
+
+import os
+from src.pdf_parser import process_all_pdfs
+
 if not os.path.exists("data/processed/penalties.csv"):
-    subprocess.run(["python", "src/pdf_parser.py"])
-
+    process_all_pdfs()
 
 import pdfplumber
 st.write("✅ pdfplumber is installed and working")
